126609388d3f320c825472b7d1fbcdc999a1e10d
AJ Lindner <alexjasonlindner@gmail.com>
2019-09-11 18:33:48 -0500

    File Upload

---
+++ PSCustomObject Report Example Script.ps1
@@ -0,0 +1,22 @@
+﻿$employeeData = import-csv C:\files\EmployeeData.csv
+$departmentData = import-csv C:\files\DepartmentData.csv
+$jobdata = import-csv C:\files\JobData.csv
+
+$outputReport = foreach ($employee in $employeeData) {
+
+    $ID = $employee.ID
+    $department = $departmentData | where DepartmentCode -eq $employee.DepartmentCode | select -expand DepartmentName
+    $jobTitle = $jobdata | where JobCode -eq $employee.JobCode | select -expand JobTitle
+    $weeklyPay = $employee.salary / 52
+
+    [pscustomobject][ordered]@{
+        ID = $ID
+        'Job Title' = $jobTitle
+        Department = $department
+        'Weekly Pay' = $weeklyPay
+    }
+}
+
+$outputReport
+
+$outputReport | export-csv c:\files\OutputReport.csv -NoTypeInformation
